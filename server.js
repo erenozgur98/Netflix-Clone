ed36266a002e3323a0b777b0358f5b71ffa5f91a
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-19 16:00:38 -0400

    added the server information

--- server.js
+++ server.js
@@ -4,3 +4,20 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/state-tracker', {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// routes
+app.use(require('./routes/api'));
+
+app.listen(PORT, () => {
+    console.log(`App running on port http://localhost:${PORT}`);
+});
