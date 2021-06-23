70695c9eb69306a4e733faa58cf2940228ae9e3e
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-23 18:13:11 -0400

    added necessary contxt inside server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const LocalStrategy = require('passport-local');
 const passportLocalMongoose = require('passport-local-mongoose');
 const User = require('./models/User');
@@ -18,13 +18,30 @@ const fs = require('fs');
 require('dotenv').config();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(require('express-session')({
+    secret: 'BIG SECRET',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.json());
 app.use(express.static('public'));
 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(Use.deserializeUser());
+
 const PORT = process.env.PORT || 3000;
 
+// Routes
+
 app.get('/', (req, res) => {
-    fs.readFile('states.json', function(err, data) {
+    fs.readFile('states.json', function (err, data) {
         if (err) {
             res.status(500).end();
         } else {
@@ -39,14 +56,37 @@ app.get('/signup', (req, res) => {
     res.render('signup.ejs');
 });
 
+app.post('/signup', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    User.register(new User({ username: username }),
+        password, (err, user) => {
+            if (err) {
+                console.log(err);
+                return res.render('signup');
+            }
+
+            passport.authenticate("local")(
+                req, res, function () {
+                    res.render("states");
+                });
+        })
+
+})
+
 app.get('/login', (req, res) => {
     res.render('login.ejs');
 });
 
-app.post('/signup', (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}), (req, res) => {
+});
 
+app.get('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/');
 })
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
