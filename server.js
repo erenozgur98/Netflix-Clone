f64c74729ee9f0ee19ed9455f1764f66cd07b0b7
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-23 17:16:47 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ require('dotenv').config();
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
