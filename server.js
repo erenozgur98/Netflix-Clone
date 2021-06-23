d294faa199132e147fc82743052256bc0f135917
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-23 18:04:43 -0400

    adding server necessary files for mongoose database

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const LocalStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
+const User = require('./models/User');
+
+mongoose.connect('mongodb://localhost/state-app', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
 const app = express();
 const fs = require('fs');
 require('dotenv').config();
@@ -28,5 +42,11 @@ app.get('/signup', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login.ejs');
 });
+
+app.post('/signup', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+})
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
