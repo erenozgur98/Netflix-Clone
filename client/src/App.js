eb5db68eb91149b4a8cf0a9141a276307f301d20
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-30 12:01:32 -0400

    added fetchUrl so we can fetch the urls inside row

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 
 function App() {
   return (
     <div className="App">
-      <Row title='Netflix Originals'/>
-      <Row title='Trending Now'/>
+      <Row title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
