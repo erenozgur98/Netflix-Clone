f5487a148d8db93b4364852cbceb9e7b8121c72d
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-30 11:52:16 -0400

    added row

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+import Row from './Row';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Row title='Netflix Originals'/>
+      <Row title='Trending Now'/>
     </div>
   );
 }
