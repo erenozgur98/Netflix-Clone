718effe8dc07fdc36b4e4acaf35018db3a9732e7
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-30 12:30:40 -0400

    adding styles

--- client/src/Row.js
+++ client/src/Row.js
@@ -1,42 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import axios from './axios';
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-const base_url = 'https://image.tmdb.org/t/p/w300/';
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    // A snipped of code which runs based on a specific condition/variable
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        };
-        fetchData();
-    }, [fetchUrl]);
+  // A snipped of code which runs based on a specific condition/variable
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    console.table(movies)
+  console.table(movies);
 
-    return (
-        <div className='row'>
-            <h2>{title}</h2>
+  return (
+    <div className="row">
+      <h2>{title}</h2>
 
-            <div className='row-posters'>
-                {/* several row-poster(s) */}
+      <div className="row-posters">
+        {/* several row-poster(s) */}
 
-                {movies.map(movie => (
-                    <img
-                        className='row-poster'
-                        src={`${base_url}${movie.poster_path}`}
-                        alt={movie.name}
-                    />
-                ))}
-
-            </div>
-            {/* container -> posters */}
-        </div>
-    )
+        {movies.map((movie) => (
+          <img
+            className="row-poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {/* container -> posters */}
+    </div>
+  );
 }
 
-export default Row
+export default Row;
