f53067d84701986302cd9d99a9c09eb43c29f823
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-30 12:24:07 -0400

    getting the data now

--- client/src/Row.js
+++ client/src/Row.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios';
 
+const base_url = 'https://image.tmdb.org/t/p/w300/';
+
 function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
 
@@ -8,17 +10,30 @@ function Row({ title, fetchUrl }) {
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log(request.data.results);
+            setMovies(request.data.results);
             return request;
-        }
-
+        };
         fetchData();
-    }, []);
+    }, [fetchUrl]);
+
+    console.table(movies)
 
     return (
-        <div>
+        <div className='row'>
             <h2>{title}</h2>
 
+            <div className='row-posters'>
+                {/* several row-poster(s) */}
+
+                {movies.map(movie => (
+                    <img
+                        className='row-poster'
+                        src={`${base_url}${movie.poster_path}`}
+                        alt={movie.name}
+                    />
+                ))}
+
+            </div>
             {/* container -> posters */}
         </div>
     )
