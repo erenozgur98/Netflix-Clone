1c359d5d8cdc595290a5c100d1eaa8db1f46666f
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-30 23:59:33 -0400

    added videos

--- client/src/Row.js
+++ client/src/Row.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
 import YouTube from "react-youtube";
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/w200/";
 
@@ -25,7 +26,22 @@ function Row({ title, fetchUrl, isLargeRow }) {
     playerVars: {
       autoplay:1,
     }
-  }
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || '')
+      .then(url => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get('v'));
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  };
 
   return (
     <div className="row">
@@ -37,6 +53,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row-poster ${isLargeRow && "row-posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
