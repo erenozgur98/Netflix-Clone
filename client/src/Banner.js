fc0b97e531bec7064dd1b549a85290f597f24939
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-30 23:00:02 -0400

    adding styles to the banner

--- client/src/Banner.js
+++ client/src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import './Banner.css'
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -26,19 +27,22 @@ function Banner() {
       style={{
         backgroundSize: 'cover',
         backgroundImage: `url(
-          'https://image.tmdb.org/t/p/original/${movie?.backdrop_path}
+          'https://image.tmdb.org/t/p/original/${movie?.backdrop_path}'
         )`,
         backgroundPosition: 'center center'
       }}
     >
       <div className="banner-contents">
-        {/* << background image */}
-        {/* Title */}
-        <h1>
+        <h1 className='banner-title'>
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
-        {/* div 2 buttons */}
-        {/* description */}
+        <div className='banner-buttons'>
+          <button className='banner-button'>Play</button>
+          <button className='banner-button'>My List</button>
+        </div>
+        <h1 className='banner-description'>
+          {movie?.overview}
+        </h1>
       </div>
     </header>
   );
